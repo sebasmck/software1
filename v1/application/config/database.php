9e98b405a2f69c8391ad0cf7f1093cefe4b58a83
sebasmck <sfarias@unbosque.edu.co>
2017-11-12 10:19:14 -0500

    added user interface to project, please check gui folder

--- v1/application/config/database.php
+++ v1/application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => '127.0.0.1',
+	'username' => 'root',
+	'password' => 'password1',
+	'database' => 'recaudo',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
