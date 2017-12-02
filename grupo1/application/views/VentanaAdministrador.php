4851d51ee6e9de38b2b685ae1baaed8feea183c9
sebasmck <sfarias@unbosque.edu.co>
2017-12-01 22:57:36 -0500

    Edited menu and updated options
    Edited menu and updated options

--- grupo1/application/views/VentanaAdministrador.php
+++ grupo1/application/views/VentanaAdministrador.php
@@ -36,26 +36,7 @@
 	<div id="sidebar"><a href="#" class="visible-phone"><i class="icon icon-home"></i> Dashboard</a>
 		<ul>
 			<li class="active"><a href="index.html"><i class="icon icon-home"></i> <span>Dashboard</span></a> </li>
-			<li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>Forms</span> <span class="label label-important">3</span></a>
-				<ul>
-					<li><a href="form-common.html">Basic Form</a></li>
-					<li><a href="form-validation.html">Form with Validation</a></li>
-					<li><a href="form-wizard.html">Form with Wizard</a></li>
-				</ul>
-			</li>
-
-			<li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>Forms</span> <span class="label label-important">3</span></a>
-				<ul>
-					<li><a href="form-common.html">Basic Form</a></li>
-					<li><a href="form-validation.html">Form with Validation</a></li>
-					<li><a href="form-wizard.html">Form with Wizard</a></li>
-				</ul>
-			</li>
-
-
-
-
-
+			<li class=""><a href="index.html"><i class="icon icon-th-list"></i> <span>Subir archivo</span></a> </li>
 		</ul>
 	</div>
 
